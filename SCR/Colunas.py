7d09e766c0ba1165ac1b9a9d5e0e3ee122d38c37
Arthur Fagundes <134743564+fahgundin@users.noreply.github.com>
2025-08-25 14:31:00 -0400

    Add files via upload

---
+++ SCR/Colunas.py
@@ -0,0 +1,46 @@
+
+import ifcopenshell
+import ifcopenshell.util.element
+from SCR.Product import *
+import pandas as pd
+from ifcopenshell.geom import create_shape
+from ifcopenshell.util.shape import get_footprint_area
+import numpy as np
+
+
+def executeColunas():
+    model = ifcopenshell.open('25-007-PRG-CR5-CONC-R00.IFC')
+    element = ifcopenshell.util.element
+
+    product = Product()
+
+    settings = ifcopenshell.geom.settings()
+    settings.set(settings.USE_WORLD_COORDS, True)
+
+
+    columns = model.by_type('IfcColumn')
+
+    coluna_name = []
+    coluna_area_forma = []
+    classe_concreto, cobrimento = [], []
+
+    for coluna in columns:
+        shape = create_shape(settings,coluna)
+        coluna_name.append(coluna.Name)
+        product.append_Element(classe_concreto, element, coluna, 'AltoQi_Eberick_Padrão','Classe de concreto',False)
+        product.append_Element(cobrimento, element, coluna, 'AltoQi_Eberick_Padrão','Cobrimento',False)
+        coluna_area_forma.append(round((get_footprint_area(shape.geometry)) * product.get_height(shape),2))
+    coluna_name.append('TOTAL')
+    classe_concreto.append('')
+    cobrimento.append('')
+    coluna_area_forma.append(round(sum(coluna_area_forma),2))
+
+    dic = {
+        'Nome': coluna_name,
+        'Classe de concreo': classe_concreto,
+        'Cobrimento': cobrimento,
+        'Área forma m²': coluna_area_forma 
+    }
+
+    df = pd.DataFrame(dic)
+    df.to_csv('./results/colunas.csv', sep=';',encoding='latin1',index=False)
