6016a23dc70f1632acc9ed834244763e8413a54e
Arthur Fagundes <134743564+fahgundin@users.noreply.github.com>
2025-08-29 15:13:08 -0400

    Add files via upload

---
+++ Estrutural/SRC/Blocos.py
@@ -0,0 +1,15 @@
+
+from SCR.abstract.Footings import *
+
+class Bloco(Footings):
+
+    def __init__(self, object):
+        self.id = object.id()
+    
+    @property
+    def _id(self):
+        return self.id
+    @_id.setter
+    def _id(self, object):
+        self.id = object
+    
---
+++ Estrutural/SRC/Sapatas.py
@@ -0,0 +1,15 @@
+
+from SCR.Footings import *
+
+class Sapatas(Footings):
+
+    def __init__(self, object):
+        self.id = object.id()
+    
+    @property
+    def _id(self):
+        return self.id
+    @_id.setter
+    def _id(self, object):
+        self.id = object
+    
